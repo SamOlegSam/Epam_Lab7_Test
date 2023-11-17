20639f3f01f0ecdcb79f48c981009f3e0edda610
Oleg <sov@transoil.gomel.by>
2023-11-17 15:13:32 +0300

    17-11-2023

---
+++ Lab7Test.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Lab7Test;
+
+namespace Lab7Test
+{
+    [TestClass]
+    public class Lab7Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {             
+            Vector a = new Vector();
+            Vector b = new Vector();
+            Vector c = new Vector();
+
+            a.x1 = 1; a.y1 = 2; a.z1 = 3;
+            a.x2 = 4; a.y2 = 5; a.z2 = 6;
+
+            b.x1 = 3; b.y1 = 4; b.z1 = 5;
+            b.x2 = 6; b.y2 = 7; b.z2 = 8;
+
+            c.x1 = 3; c.y1 = 4; c.z1 = 5;
+            c.x2 = 6; c.y2 = 7; c.z2 = 8;
+
+            Vector d = new Vector();
+            Vector actual = d.Plus(a, b);
+
+            Assert.AreEqual(c, actual);
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Lab7Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lab7Test")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cc4f7f2b-dc77-4aba-b5ea-66363f5accbc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
